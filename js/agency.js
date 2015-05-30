a0ef09bba1425b043e2b9c808f486a17a3ab2e49
aaackerman <aaackerman@yahoo.com>
2015-05-29 21:59:49 -0400

    Add particles

--- js/agency.js
+++ js/agency.js
@@ -13,6 +13,37 @@ $(function() {
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
+
+    particlesJS('particles', {
+      particles: {
+        color: '#fff',
+        shape: 'circle',
+        opacity: 1,
+        size: 2.5,
+        size_random: true,
+        nb: 100,
+        line_linked: {
+          enable_auto: true,
+          distance: 250,
+          color: '#fff',
+          opacity: 0.5,
+          width: 1.5,
+          condensed_mode: {
+            enable: false,
+            rotateX: 600,
+            rotateY: 600
+          }
+        },
+        anim: {
+          enable: true,
+          speed: 2
+        }
+      },
+      interactivity: {
+        enable: true
+      },
+      retina_detect: true
+    });
 });
 
 // Highlight the top nav as scrolling occurs
